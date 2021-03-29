e9e22f48bb698dd5f2ca99cc73fba771532a3fba
Asha <paramonpav@yandex.ru>
2021-03-29 18:39:44 +0300

    feat: Routing git add -Agit add -A

--- yandex-diploma/src/App.js
+++ yandex-diploma/src/App.js
@@ -7,16 +7,32 @@ import Profile from './components/Profile';
 import Register from './components/Register';
 import Login from './components/Login'
 import ErrorPage from './components/ErrorPage.js'
-
+import { Route, Switch } from 'react-router-dom'
 function App() {
   return (
-    //  <Main></Main>
-    // <Movies></Movies>
-    // <SavedMovies></SavedMovies>
-    // <Profile></Profile>
-    // <Register></Register>
-    // <Login></Login>
-    <ErrorPage code="404" message="Страница не найдена"></ErrorPage>
+    <Switch>
+      <Route exact path="/">
+        <Main></Main>
+      </Route>
+      <Route path="/movies">
+        <Movies></Movies>
+      </Route>
+      <Route path="/saved-movies">
+        <SavedMovies></SavedMovies>
+      </Route>
+      <Route path="/profile">
+        <Profile></Profile>
+      </Route>
+      <Route path="/signup">
+        <Register></Register>
+      </Route>
+
+      <Route path="/signin">
+        <Login></Login>
+      </Route>
+
+      <ErrorPage code="404" message="Страница не найдена"></ErrorPage>
+    </Switch>
   );
 }
 
